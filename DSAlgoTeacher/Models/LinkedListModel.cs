ef4c9388497fda7e877f5e1d4b5e22971e727370
Masida Gondwe <Masida.Gondwe@fqml.com>
2025-05-30 22:18:47 +0200

    getting back at it

---
+++ DSAlgoTeacher/Models/LinkedListModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgoTeacher.Models
+{
+    public class LinkedListModel
+    {
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+        public Node Head { get; private set; }
+        public LinkedListModel()
+        {
+            Head = null;
+        }
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+        public void Display()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
---
+++ DSAlgoTeacher/Models/QueueModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgoTeacher.Models
+{
+    public class QueueModel
+    {
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+        private Node front;
+        private Node rear;
+        public QueueModel()
+        {
+            front = null;
+            rear = null;
+        }
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (rear == null)
+            {
+                front = rear = newNode;
+                return;
+            }
+            rear.Next = newNode;
+            rear = newNode;
+        }
+        public int Dequeue()
+        {
+            if (front == null)
+                throw new InvalidOperationException("Queue is empty.");
+            
+            int data = front.Data;
+            front = front.Next;
+            if (front == null)
+                rear = null;
+            return data;
+        }
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+    }
+}
